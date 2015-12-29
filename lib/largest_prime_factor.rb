2ca6869d490701d7c338f2d50a6dc3dcc8a267ca
chintas1 <chintas1@tcnj.edu>
2015-12-29 16:48:48 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,5 @@
+require 'prime'
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+  (Prime.prime_division(num))[-1][0]
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,12 @@
+require 'prime'
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  @number = 0
+  def initialize(num)
+    @number = num
+  end
+
+  def number
+    (Prime.prime_division(@number))[-1][0]
+  end
+end
